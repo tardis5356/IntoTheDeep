3d7e8bdbc1c8671ea736ea42cb833e763392b61b
Eavan <eavanqin0716@gmail.com>
2024-10-03 18:26:19 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testbed/TestbedAuto.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.Testbed;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+
+
+@Config
+@Autonomous(name = "TestBedAuto", group = "Autonomous")
+public class TestbedAuto extends LinearOpMode {
+    @Override
+
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(-15, -64, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        // vision here that outputs position
+        int visionOutputPosition = 1;
+
+
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .strafeTo(new Vector2d(-15, -60));
+        // .strafeTo(new Vector2d(36, -4));
+        //.lineToX(30)
+        //.lineToY(-4)
+        Action trajectoryActionCloseOut = tab1.fresh()
+//                .strafeTo(new Vector2d(30, 32))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-5,-60))
+                .build();
+
+
+
+//    trajectoryAction2 = drive.actionBuilder(drive.pose)
+//            .turn(Math.toRadians(90))
+//            .lineToY(-48)
+//            .lineToX(48)
+//            .build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            int position = visionOutputPosition;
+            telemetry.addData("Position during Init", position);
+            telemetry.update();
+        }
+
+        int startPosition = visionOutputPosition;
+        telemetry.addData("Starting Position", startPosition);
+        telemetry.update();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action trajectoryActionChosen;
+        trajectoryActionChosen = tab1.build();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen,
+                        trajectoryActionCloseOut
+                )
+        );
+    }
+}
