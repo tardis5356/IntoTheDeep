6d8361ece037d5abfba7d1f2d61f0da89b42a950
Thang <ftctardis@gmail.com>
2024-11-04 20:18:11 -0500

    Subsystem Poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
@@ -7,5 +7,5 @@ public class BotPositions {
     public static double GRIPPER_OPEN = .3, GRIPPER_CLOSED = 0, GRIPPER_INTAKE = .15;
     public static double WRIST_TRANSIT = 0, WRIST_WALL = .2, WRIST_INTAKE = .3, WRIST_BASKET = .7;
     public static double ARM_BASKET = 1, ARM_INTAKE = .4, ARM_SPECIMEN = .7, ARM_TRANSIT = .5;
-    public static double INTAKE_NEUTRAL = 1, INTAKE_DOWN = 0.75, INTAKE_IN = 1, INTAKE_OUT = -1;
+    public static double INTAKE_NEUTRAL = 0, INTAKE_DOWN = 0.3, INTAKE_IN = 1, INTAKE_OUT = -1;
 }
