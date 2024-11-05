c20c0fa64777b9df16c02234ff485c830dc145e9
tardis5356 <ftctardis@gmail.com>
2024-11-04 20:00:44 -0500

    Added Extendo class. not programmed yet - Jacob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/Extendo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/Extendo.java
@@ -2,5 +2,5 @@ package org.firstinspires.ftc.teamcode.Andie.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 
-public class Extendo extends SubsystemBase {
+public class Extendo extends SubsystemBase{
 }
