853c3bb1d1701e5251046a2cab66b3bbe27378c0
Thang <ftctardis@gmail.com>
2024-11-10 16:04:16 -0500

    RobotToStateCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
@@ -8,5 +8,6 @@ public class BotPositions {
     public static double WRIST_TRANSIT = 0, WRIST_WALL = .2, WRIST_INTAKE = .3, WRIST_BASKET = .7;
     public static double ARM_BASKET = 1, ARM_INTAKE = .4, ARM_SPECIMEN = .7, ARM_TRANSIT = .5;
     public static double INTAKE_NEUTRAL = 0, INTAKE_DOWN = 0.3, INTAKE_IN = 1, INTAKE_OUT = -1;
+    public static double EXTENDO_IN = 1, EXTENDO_OUT = 0, EXTENDO_MIDDLE;
     public static int LIFT_WALL = 10, LIFT_SPECIMEN_HIGH = 40, LIFT_SPECIMEN_LOW = 20, LIFT_BASKET_HIGH = 50, LIFT_BASKET_LOW = 20;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/Extendo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/Extendo.java
@@ -1,6 +1,40 @@
 package org.firstinspires.ftc.teamcode.Andie.Subsystems;
 
+import static org.firstinspires.ftc.teamcode.Andie.Subsystems.BotPositions.EXTENDO_IN;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Extendo extends SubsystemBase {
+    private Servo sER, sEL;
+    double extensionPosition;
+    double stickInput;
+
+    public Extendo(HardwareMap hardwareMap, double stickPosition){
+        sER = hardwareMap.get(Servo.class, "sER");
+        sEL = hardwareMap.get(Servo.class, "sEL");
+        stickInput = stickPosition;
+    }
+
+    @Override
+
+    public void periodic(){
+        sER.setPosition(extensionPosition);
+        sEL.setPosition(extensionPosition);
+        extensionPosition += stickInput;
+    }
+
+    public void extendoIn(){
+        extensionPosition = EXTENDO_IN;
+        extensionPosition = EXTENDO_IN;
+    }
+    public void extendoOut(){
+        sER.setPosition(BotPositions.EXTENDO_OUT);
+        sEL.setPosition(BotPositions.EXTENDO_OUT);
+    }
+    public void extendoMiddle(){
+        sER.setPosition(BotPositions.EXTENDO_MIDDLE);
+        sEL.setPosition(BotPositions.EXTENDO_MIDDLE);
+    }
 }
