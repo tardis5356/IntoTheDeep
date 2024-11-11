67564b8c6777eba9f223efdfc5aca6ddbac5c48b
tardis5356 <ftctardis@gmail.com>
2024-11-11 14:14:52 -0500

    idk tbh 11/10/24 -Victor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/TeleOps/IndexTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/TeleOps/IndexTeleop.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Andie.TeleOps;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.Trigger;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
@@ -9,7 +10,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.Andie.Commands.LiftToStateCommand;
 import org.firstinspires.ftc.teamcode.Andie.Subsystems.Arm;
+import org.firstinspires.ftc.teamcode.Andie.Subsystems.BotPositions;
 import org.firstinspires.ftc.teamcode.Andie.Subsystems.Extendo;
 import org.firstinspires.ftc.teamcode.Andie.Subsystems.Gripper;
 import org.firstinspires.ftc.teamcode.Andie.Subsystems.Intake;
@@ -17,8 +20,8 @@ import org.firstinspires.ftc.teamcode.Andie.Subsystems.Lift;
 import org.firstinspires.ftc.teamcode.Andie.Subsystems.Wrist;
 
 @TeleOp(name="indexTele")
-@Disabled
-public class IndexTeleop extends LinearOpMode {
+//@Disabled
+public class IndexTeleop extends CommandOpMode {
 
     //gamepads
     private GamepadEx driver1, driver2;
@@ -52,7 +55,7 @@ public class IndexTeleop extends LinearOpMode {
 
 
     @Override
-    public void runOpMode(){
+    public void initialize() {
         //init controllers
         driver1 = new GamepadEx(gamepad1);
         driver2 = new GamepadEx(gamepad2);
@@ -74,10 +77,6 @@ public class IndexTeleop extends LinearOpMode {
 
         //extendo
         arm = new Arm(hardwareMap);
-
-
-        waitForStart();
-        while(opModeIsActive()){
 
             //arm
             new Trigger(() -> driver1.getButton(GamepadKeys.Button.X))
@@ -118,6 +117,43 @@ public class IndexTeleop extends LinearOpMode {
 
             new Trigger(() -> driver1.getButton(GamepadKeys.Button.RIGHT_STICK_BUTTON))
                     .whenActive(new InstantCommand(extendo::extendoOut));
+
+            new Trigger(() -> driver2.getButton(GamepadKeys.Button.DPAD_UP))
+                    .whenActive(new LiftToStateCommand(lift, BotPositions.LIFT_BASKET_HIGH, 20));
+
+        new Trigger(() -> driver2.getButton(GamepadKeys.Button.A))
+                .toggleWhenActive(new InstantCommand(gripper::closeGripper), new InstantCommand(gripper::openGripper));
+
+        new Trigger(() -> driver2.getButton(GamepadKeys.Button.B))
+                .whenActive(new InstantCommand(gripper::intakeGripper));
         }
+
+    public void run() {
+        super.run();
+
+        Rotation = cubicScaling(-gamepad1.right_stick_x);
+        FB = cubicScaling(gamepad1.left_stick_y);
+        LR = cubicScaling(-gamepad1.left_stick_x) * 1.2;
+
+        //defines the powers for the motors (trust i've written this so many times)
+        double mFLPower = FB + LR + Rotation;
+        double mFRPower = FB - LR - Rotation;
+        double mBLPower = FB - LR + Rotation;
+        double mBRPower = FB + LR - Rotation;
+
+        mFL.setPower(mFLPower * CURRENT_SPEED_MULTIPLIER);
+        mFR.setPower(mFRPower * CURRENT_SPEED_MULTIPLIER);
+        mBL.setPower(mBLPower * CURRENT_SPEED_MULTIPLIER);
+        mBR.setPower(mBRPower * CURRENT_SPEED_MULTIPLIER);
+    }
+    private double cubicScaling(float joystickValue) {
+        double v = 0.05 * joystickValue + 0.95 * Math.pow(joystickValue, 3);
+        if (joystickValue > 0.02)
+            return 0.1 + v;
+        else if (joystickValue < -0.02)
+            return -0.1 + v;
+        else
+            return 0;
     }
 }
+
