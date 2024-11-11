76faf813951b0683fbfae90c7affe7218d8bb427
Eavan <eavanqin0716@gmail.com>
2024-11-11 13:05:21 -0500

    2024/11/11 tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed/Tuning/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed/Tuning/MecanumDrive.java
@@ -66,7 +66,7 @@ public class MecanumDrive {
         // drive model parameters
         public double inPerTick =0.0005307; // 0.00068991, 0.00053065
         public double lateralInPerTick = 0.00043042668409853835; //0.00042145826939852453
-        public double trackWidthTicks = 24435.89234696338; //24180.892621514482; 24115.12709698697
+        public double trackWidthTicks = 24611.417305220926; //24180.892621514482
 
         // feedforward parameters (in tick units)
         public double kS = 0.922014230665804 ; //0.8306274788948262
