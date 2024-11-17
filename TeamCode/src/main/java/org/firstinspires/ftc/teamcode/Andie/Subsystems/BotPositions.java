22f371dd117f1146d59930e7e4e5ee7eaf09573b
tardis5356 <ftctardis@gmail.com>
2024-11-17 09:45:15 -0500

    The Lift lifts!!!!! 11/16/2024 -victor roussev

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Andie/Subsystems/BotPositions.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class BotPositions {
-    public static double GRIPPER_OPEN = .3, GRIPPER_CLOSED = 0.19, GRIPPER_INTAKE = .225;
+    public static double GRIPPER_OPEN = .3, GRIPPER_CLOSED = 0.19, GRIPPER_INTAKE = .28;
     public static double WRIST_TRANSIT = 0.87, WRIST_WALL = .75, WRIST_INTAKE = .67, WRIST_BASKET = .46;
     public static double ARM_BASKET = .98, ARM_INTAKE = .323, ARM_SPECIMEN = .1, ARM_TRANSIT = .5;
     public static double INTAKE_UP = 0.079, INTAKE_DOWN = 0.237, INTAKE_IN = 1, INTAKE_OUT = -1, INTAKE_STOP = 0;
