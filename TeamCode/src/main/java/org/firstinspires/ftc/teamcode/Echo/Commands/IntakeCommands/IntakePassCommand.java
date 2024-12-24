a7f3e98736457ae4c1f96da263c3680087aea637
Thang <ftctardis@gmail.com>
2024-12-23 20:29:35 -0500

    12/23/24 Started work on automating the pass of the intake and changing triggers in teleop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Echo/Commands/IntakeCommands/IntakePassCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Echo/Commands/IntakeCommands/IntakePassCommand.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Echo.Commands.IntakeCommands;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.Echo.Subsystems.Intake;
 
@@ -9,9 +10,9 @@ public class IntakePassCommand extends SequentialCommandGroup {
     public IntakePassCommand (Intake intake) {
         addCommands(
                 new InstantCommand(()->intake.samplePresent = false),
-                new InstantCommand(intake::transfer)//,
-                //new WaitCommand(750),
-                //new InstantCommand(intake::stop)
+                new InstantCommand(intake::transfer),
+                new WaitCommand(750),
+                new InstantCommand(intake::stop)
         );
     }
 }
